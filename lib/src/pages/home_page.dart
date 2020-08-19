b32b3c50fc17ef02fbe4a645987d1b4b4f8f9e00
seront <seront.nmmc@gmail.com>
2020-08-19 14:32:16 -0400

    Leccion 37 - instalacion y preparacion swiper

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -6,7 +7,41 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('Home page'),) 
+      appBar: AppBar(
+        title: Text('Peliculas en Cines'),
+        centerTitle: false,
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search), 
+            onPressed: (){},
+            )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _swiperTarjetas(),
+        ],),
+        ) 
+    );
+  }
+
+  Widget _swiperTarjetas() {
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      width: double.infinity,
+      height: 300,
+      child: Swiper(
+          layout: SwiperLayout.STACK,
+          itemBuilder: (BuildContext context,int index){
+            return new Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
+          },
+          itemCount: 3,
+          itemWidth: 250,
+          pagination: new SwiperPagination(),
+          control: new SwiperControl(),
+        ),
     );
   }
 }
