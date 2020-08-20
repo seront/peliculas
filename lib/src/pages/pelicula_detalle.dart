678537c1b241038789c4d938f081e6c2e03f5a5f
seront <seront.nmmc@gmail.com>
2020-08-20 19:42:12 -0400

    113: agregando estilo al appbar del detalle

--- lib/src/pages/pelicula_detalle.dart
+++ lib/src/pages/pelicula_detalle.dart
@@ -11,8 +11,34 @@ class PeliculaDetalle extends StatelessWidget {
     final Pelicula pelicula = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
-      body: Center(
-        child: Text(pelicula.title),
+      body:  CustomScrollView(
+        slivers: <Widget>[
+          _crearAppbar(pelicula)
+        ],
+      ),
+    );
+  }
+
+  Widget _crearAppbar(Pelicula pelicula) {
+    return SliverAppBar(
+      elevation: 2.0,
+      backgroundColor: Colors.indigoAccent,
+      expandedHeight: 200.0,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        title: Text(
+          pelicula.title,
+          style: TextStyle(color: Colors.white, fontSize: 16.0),
+        ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/img/loading.gif'), 
+          image: NetworkImage(pelicula.getBackgroundImg()),
+          fadeInDuration: Duration(milliseconds: 150),
+          fit: BoxFit.cover,
+          ),
+          
       ),
     );
   }
