c98ede6877c439c78db88b28e2951a3bb95ef917
seront <seront.nmmc@gmail.com>
2020-08-20 19:58:02 -0400

    115: estilos y elementos, parte media,detalle pelicula

--- lib/src/pages/pelicula_detalle.dart
+++ lib/src/pages/pelicula_detalle.dart
@@ -13,7 +13,20 @@ class PeliculaDetalle extends StatelessWidget {
     return Scaffold(
       body:  CustomScrollView(
         slivers: <Widget>[
-          _crearAppbar(pelicula)
+          _crearAppbar(pelicula),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                SizedBox(height: 10.0,),
+                _posterTitulo(context, pelicula),
+                _descripcion(pelicula),
+                _descripcion(pelicula),
+                _descripcion(pelicula),
+                _descripcion(pelicula),
+                _descripcion(pelicula),
+              ]
+            )
+            )
         ],
       ),
     );
@@ -39,6 +52,49 @@ class PeliculaDetalle extends StatelessWidget {
           fit: BoxFit.cover,
           ),
           
+      ),
+    );
+  }
+
+  Widget _posterTitulo(BuildContext context, Pelicula pelicula) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+                      child: Image(
+              image: NetworkImage(pelicula.getPosterImg()),
+              height: 150.0,
+              ),
+          ),
+          SizedBox(width: 20.0,),
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(pelicula.title, style: Theme.of(context).textTheme.title, overflow: TextOverflow.ellipsis,),
+                Text(pelicula.originalTitle, style: Theme.of(context).textTheme.subhead, overflow: TextOverflow.ellipsis,),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.star_border),
+                    Text(pelicula.voteAverage.toString(), style: Theme.of(context).textTheme.subhead, overflow: TextOverflow.ellipsis)
+                  ]
+                )
+              ],
+            ),
+            )
+        ],
+      ),
+    );
+  }
+
+  Widget _descripcion(Pelicula pelicula) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+      child: Text(
+        pelicula.overview,
+      textAlign: TextAlign.justify,
       ),
     );
   }
