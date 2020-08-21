717c57cf60fbb20ac9895389d2e54f7672c1fb4d
seront <seront.nmmc@gmail.com>
2020-08-20 21:49:02 -0400

    122: busqueda en lista local estatica

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 
 class DataSearch extends SearchDelegate{
+
+  final peliculas = [
+    'Spiderman',
+    'Superman',
+    'Liga de la justicia',
+    'Avatar',
+    'Capitan America',
+  ];
+
+  final peliculasRecientes = [
+    'Spiderman',
+    'Capitan America'
+  ];
+
   @override
   List<Widget> buildActions(BuildContext context) {
     // Las cciones de nuestro AppBar
@@ -37,7 +51,22 @@ class DataSearch extends SearchDelegate{
   @override
   Widget buildSuggestions(BuildContext context) {
     // Son las Sugerencias que aparecen cuando la persona escribe
-    return Container();
+
+    final listaSugerida = (query.isEmpty) ? 
+    peliculasRecientes : 
+    peliculas.where(
+      (p) => p.toLowerCase().contains(query.toLowerCase())
+    ).toList();
+
+    return ListView.builder(
+      itemCount: listaSugerida.length,
+      itemBuilder: (context, i){
+        return ListTile(
+          leading: Icon(Icons.movie),
+          title: Text(listaSugerida[i]),
+        );
+      },
+      );
   }
 
 }
